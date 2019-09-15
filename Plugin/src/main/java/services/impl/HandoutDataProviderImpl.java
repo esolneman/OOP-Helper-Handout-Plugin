38fbc1cb4ac16e6be56bb6389740d5fd1744befe
Marlena Wolfes <marlena.wolfes@gmx.de>
2019-09-15 21:04:31 +0200

    start get url from jar file c.f. #34

--- Plugin/src/main/java/services/impl/HandoutDataProviderImpl.java
+++ Plugin/src/main/java/services/impl/HandoutDataProviderImpl.java
@@ -2,6 +2,7 @@ package services.impl;
 
 import Controller.HandoutPluginController;
 import Listener.OnEventListener;
+import de.ur.mi.pluginhelper.TaskConfiguration;
 import org.apache.commons.io.FileUtils;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
@@ -38,7 +39,13 @@ public class HandoutDataProviderImpl implements HandoutDataProvider{
         projectDirectory = project.getBasePath();
         contentRepoPath = projectDirectory + CONTENT_FILE_NAME;
         contentRepoFile = new File (contentRepoPath);
+        getRepoUrl();
         System.out.println(contentRepoPath);
+    }
+
+    private void getRepoUrl() {
+        System.out.println("getRepoUrl");
+        System.out.println(TaskConfiguration.loadFrom());
     }
 
     public void updateHandoutData() {
