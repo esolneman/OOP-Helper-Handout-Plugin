b9254181cdeab1904681c6eebd798700cdbaab0b
Marlena Wolfes <marlena.wolfes@gmx.de>
2019-09-29 18:12:17 +0200

    prepare restart merge

--- Plugin/src/main/java/tasks/UpdateTask.java
+++
@@ -1,9 +0,0 @@
-package tasks;
-
-public class UpdateTask {
-    public String repoURL;
-    public String branchName;
-    public String cloneRepositoryPath;
-    public String localStorageFileName;
-
-}
