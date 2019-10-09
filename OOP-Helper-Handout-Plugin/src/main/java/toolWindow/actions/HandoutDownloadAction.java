ea3a04d30cce2c882fcddcc027ce59a5ac8eadcb
Marlena Wolfes <marlena.wolfes@gmx.de>
2019-10-09 17:53:41 +0200

    convert local html file to pdf is working #39

--- OOP-Helper-Handout-Plugin/src/main/java/toolWindow/actions/HandoutDownloadAction.java
+++ OOP-Helper-Handout-Plugin/src/main/java/toolWindow/actions/HandoutDownloadAction.java
@@ -4,17 +4,17 @@ import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
+import com.sun.org.apache.xpath.internal.operations.Div;
 import io.woo.htmltopdf.HtmlToPdf;
 import io.woo.htmltopdf.HtmlToPdfObject;
 import org.jsoup.Jsoup;
 import provider.LocalStorageDataProvider;
 import provider.RepoLocalStorageDataProvider;
-import java.io.File;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
+
 import java.net.MalformedURLException;
+import java.net.URL;
 
 import static environment.Constants.LOCAL_STORAGE_FILE;
 import static environment.Constants.REPO_LOCAL_STORAGE_FILE;
@@ -31,23 +31,21 @@ public class HandoutDownloadAction extends AnAction {
         System.out.println("DownloadAction started");
         String handoutHTMLDirectory = RepoLocalStorageDataProvider.getHandoutHtmlString();
         String handoutPDFDirectory = RepoLocalStorageDataProvider.getUserProjectDirectory() + LOCAL_STORAGE_FILE + REPO_LOCAL_STORAGE_FILE + "/handout.pdf";
-        createPDF(handoutHTMLDirectory, handoutPDFDirectory);
+        create (handoutHTMLDirectory, handoutPDFDirectory);
     }
 
-    private void createPDF(String input, String output){
+    private void create(String handoutHTMLDirectory, String handoutPDFDirectory) {
+        System.out.println(handoutHTMLDirectory);
         File content = LocalStorageDataProvider.getHandoutFileDirectory();
         try {
             String urlString = content.toURI().toURL().toString();
             System.out.println(urlString);
-
             boolean success = HtmlToPdf.create()
-                    .object(HtmlToPdfObject.forUrl(urlString))
-                    .convert("/path/to/file.pdf");
+                    .object(HtmlToPdfObject.forUrl("file:///" + handoutHTMLDirectory))
+                    .convert(handoutPDFDirectory);
             System.out.println(success);
         } catch (MalformedURLException e) {
             e.printStackTrace();
-            System.out.println(e);
-
         }
 
 
