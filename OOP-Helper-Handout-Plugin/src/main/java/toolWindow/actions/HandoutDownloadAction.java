a52a5d472f84c133e0431653e62fa2a8cf1607e5
Marlena Wolfes <marlena.wolfes@gmx.de>
2019-10-11 10:04:17 +0200

    Add FileChooser for downloading handout html #39

--- OOP-Helper-Handout-Plugin/src/main/java/toolWindow/actions/HandoutDownloadAction.java
+++ OOP-Helper-Handout-Plugin/src/main/java/toolWindow/actions/HandoutDownloadAction.java
@@ -2,6 +2,11 @@ package toolWindow.actions;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.fileChooser.FileChooser;
+import com.intellij.openapi.fileChooser.FileChooserDescriptor;
+import com.intellij.openapi.fileChooser.FileChooserDescriptorFactory;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
 import io.woo.htmltopdf.HtmlToPdf;
 import io.woo.htmltopdf.HtmlToPdfObject;
 import provider.LocalStorageDataProvider;
@@ -17,6 +22,7 @@ import static environment.Constants.REPO_LOCAL_STORAGE_FILE;
 public class HandoutDownloadAction extends AnAction {
 
     public static final String DEST = "target/results/sample.pdf";
+    private Project project;
 
     public HandoutDownloadAction() {
         super("Download");
@@ -24,18 +30,27 @@ public class HandoutDownloadAction extends AnAction {
 
     public void actionPerformed(AnActionEvent event) {
         System.out.println("DownloadAction started");
+        System.out.println(event.getPlace());
+        project = event.getProject();
         String handoutHTMLDirectory = RepoLocalStorageDataProvider.getHandoutHtmlString();
-        String handoutPDFDirectory = RepoLocalStorageDataProvider.getUserProjectDirectory() + LOCAL_STORAGE_FILE + REPO_LOCAL_STORAGE_FILE + "/handout.pdf";
-        create (handoutHTMLDirectory, handoutPDFDirectory);
+        create (handoutHTMLDirectory);
     }
 
     //https://github.com/wooio/htmltopdf-java
-    private void create(String handoutHTMLDirectory, String handoutPDFDirectory) {
+    private void create(String handoutHTMLDirectory) {
         System.out.println(handoutHTMLDirectory);
         File content = LocalStorageDataProvider.getHandoutFileDirectory();
         try {
             String urlString = content.toURI().toURL().toString();
             System.out.println(urlString);
+            final FileChooserDescriptor descriptor = FileChooserDescriptorFactory.createSingleFolderDescriptor();
+            descriptor.setTitle("Choose location for PDF");
+            descriptor.setDescription("Description for Downloading PDF");
+            descriptor.setForcedToUseIdeaFileChooser(true);
+            VirtualFile file = FileChooser
+                    .chooseFile(descriptor, project, null);
+            String handoutPDFDirectory = file.getPath() + "/handout.pdf";
+            System.out.println("handoutPDFDirectory: " + handoutPDFDirectory);
             boolean success = HtmlToPdf.create()
                     .object(HtmlToPdfObject.forUrl("file:///" + handoutHTMLDirectory))
                     .convert(handoutPDFDirectory);
