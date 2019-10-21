e0c9e43ccc089d635e1a74be10f9812ae0586c29
Marlena Wolfes <marlena.wolfes@gmx.de>
2019-10-21 13:45:27 +0200

    link to method is working from handout #47

--- OOP-Helper-Handout-Plugin/src/main/java/toolWindow/HandoutContentScreen.java
+++ OOP-Helper-Handout-Plugin/src/main/java/toolWindow/HandoutContentScreen.java
@@ -5,12 +5,9 @@ import com.intellij.openapi.actionSystem.ActionToolbar;
 import com.intellij.openapi.actionSystem.DefaultActionGroup;
 import com.intellij.openapi.actionSystem.Separator;
 import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.editor.Editor;
-import com.intellij.openapi.editor.LogicalPosition;
-import com.intellij.openapi.editor.ScrollType;
+import com.intellij.openapi.editor.*;
 import com.intellij.openapi.fileEditor.FileEditor;
 import com.intellij.openapi.fileEditor.FileEditorManager;
-import com.intellij.openapi.fileEditor.OpenFileDescriptor;
 import com.intellij.openapi.fileEditor.TextEditor;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.SimpleToolWindowPanel;
@@ -18,11 +15,14 @@ import com.intellij.openapi.vfs.LocalFileSystem;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.openapi.wm.ToolWindow;
 import environment.HandoutPluginFXPanel;
-import gherkin.lexer.Fi;
 import javafx.application.Platform;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.scene.web.WebView;
+import javassist.ClassPool;
+import javassist.CtClass;
+import javassist.CtMethod;
+import javassist.NotFoundException;
 import org.apache.commons.lang.WordUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -34,12 +34,16 @@ import toolWindow.actionGroups.HandoutContentActionGroup;
 
 import javax.swing.*;
 import java.awt.*;
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.lang.reflect.Method;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.stream.Stream;
 
 public class HandoutContentScreen extends SimpleToolWindowPanel{
     private HandoutPluginFXPanel handoutContent;
@@ -160,7 +164,6 @@ public class HandoutContentScreen extends SimpleToolWindowPanel{
                     //https://stackoverflow.com/a/17113365
                     String classDirectory = toBeopen.split("(?<=class)")[1];
                     System.out.println("classDirectory: " + classDirectory);
-
                     VirtualFile newFile = LocalFileSystem.getInstance().findFileByPath(RepoLocalStorageDataProvider.getUserProjectDirectory() + classDirectory);
                     ApplicationManager.getApplication().invokeLater(() -> {
                         //https://intellij-support.jetbrains.com/hc/en-us/community/posts/206113019-Open-a-Class-programmatically-in-a-new-Editor
@@ -169,28 +172,59 @@ public class HandoutContentScreen extends SimpleToolWindowPanel{
                 } else if (toBeopen.contains("method/")) {
                     System.out.println("Link to method: " + toBeopen);
                     String classDirectory = toBeopen.split("(?<=method)")[1];
-                    System.out.println("classDirectory: " + classDirectory);
                     Integer findLastSlash = classDirectory.lastIndexOf("/");
                     classDirectory = classDirectory.substring(0, findLastSlash);
                     System.out.println("classDirectory: " + classDirectory);
                     String methodName = toBeopen.split("(?<=/)")[toBeopen.split("(?<=/)").length -1];
                     System.out.println("methodName: " + methodName);
                     VirtualFile newFile = LocalFileSystem.getInstance().findFileByPath(RepoLocalStorageDataProvider.getUserProjectDirectory() + classDirectory);
+                    /*assert newFile != null;
+                    Class fileClass = newFile.getClass();
+                    System.out.println("fileClass: " + fileClass.getName());
+                    for (Method method : fileClass.getMethods()) {
+                        System.out.println("Class methodName: " + method.getName());
+                    }*/
+
+
+                    //File file = new File("C:/Masterarbeit/TestProjekt/OOP-18WS-CoreDefense-Starter/HelperHandoutPluginContentData/RepoLocalStorage/method/src/world/Player.java");
+                    File file = new File("C:/Masterarbeit/TestProjekt/OOP-18WS-CoreDefense-Starter/src/world/Player.java");
+                    System.out.println("file " + file.getName());
+                    int methodLineNumber = 0;
+                    try {
+                        Scanner scanner = new Scanner(file);
+                        int lineNum = 0;
+                        while (scanner.hasNextLine()) {
+                            String line = scanner.nextLine();
+                            lineNum++;
+                            if(line.contains(methodName)) {
+                                methodLineNumber = lineNum;
+                                break;
+                            }
+                        }
+                    } catch(FileNotFoundException e) {
+                        //handle this
+                    }
+
+                    int finalMethodLineNumber = methodLineNumber;
                     ApplicationManager.getApplication().invokeLater(() -> {
                         //https://intellij-support.jetbrains.com/hc/en-us/community/posts/206113019-Open-a-Class-programmatically-in-a-new-Editor
                         //FileEditorManager.getInstance(project).openFile(newFile, true, true);
-
-
                         //https://www.codota.com/code/java/methods/com.intellij.openapi.fileEditor.FileEditorManager/openFile?snippet=5ce6acf17e03440004e29f2f
                         final FileEditorManager fileEditorManager = FileEditorManager.getInstance(project);
-                        final FileEditor[] editor = fileEditorManager.openFile(
-                                newFile, true);
-                        final LogicalPosition problemPos = new LogicalPosition(20, 0);
-
+                        final FileEditor[] editor = fileEditorManager.openFile(newFile, true);
                         final Editor textEditor = ((TextEditor) editor[0]).getEditor();
-                        textEditor.
+                        final CaretModel caretModel = textEditor.getCaretModel();
+                        final Caret primaryCaret = caretModel.getPrimaryCaret();
+
+                        final LogicalPosition problemPos = new LogicalPosition(finalMethodLineNumber - 1, 0);
                         textEditor.getCaretModel().moveToLogicalPosition(problemPos);
+                        textEditor.getSelectionModel().sele
                         textEditor.getScrollingModel().scrollToCaret(ScrollType.CENTER);
+
+
+                        //showMessage("Unable to find " + filename);
+
+
                     });
 
 
