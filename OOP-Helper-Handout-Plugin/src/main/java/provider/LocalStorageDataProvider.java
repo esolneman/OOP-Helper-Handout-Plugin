afddfe55e34fa850e6f3473a9339f65a1a9a41cd
Marlena Wolfes <marlena.wolfes@gmx.de>
2019-10-25 14:31:42 +0200

    try to higlight selected text -> pause this feature #56

--- OOP-Helper-Handout-Plugin/src/main/java/provider/LocalStorageDataProvider.java
+++ OOP-Helper-Handout-Plugin/src/main/java/provider/LocalStorageDataProvider.java
@@ -3,6 +3,8 @@ package provider;
 import java.io.File;
 import java.util.ArrayList;
 
+import static environment.Constants.*;
+
 public class LocalStorageDataProvider implements LocalStorageDataProviderInterface {
 
     public static File getHandoutFileDirectory(){
@@ -12,4 +14,10 @@ public class LocalStorageDataProvider implements LocalStorageDataProviderInterfa
     public static String getHandoutStringDirectory(){
         return RepoLocalStorageDataProvider.getHandoutHtmlString();
     }
+
+    public static String testFile(){
+        String testPath = RepoLocalStorageDataProvider.getUserProjectDirectory() + LOCAL_STORAGE_FILE + REPO_LOCAL_STORAGE_FILE + "/test.html";;
+        return testPath;
+    }
+
 }
--- OOP-Helper-Handout-Plugin/src/main/java/webView/WebViewController.java
+++ OOP-Helper-Handout-Plugin/src/main/java/webView/WebViewController.java
@@ -2,10 +2,19 @@ package webView;
 
 import javafx.application.Platform;
 import javafx.scene.web.WebView;
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang.WordUtils;
-import org.w3c.dom.*;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import provider.LocalStorageDataProvider;
 
-import javax.swing.text.html.HTML;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.File;
+import java.io.IOException;
 
 public class WebViewController {
     private WebView webView;
@@ -28,9 +37,6 @@ public class WebViewController {
     //https://stackoverflow.com/questions/49070734/javafx-webview-link-to-anchor-in-document-doesnt-work-using-loadcontent
     public void goToLocation(String heading){
         System.out.println("heading: " + heading);
-
-
-
         //TODO: check or if (class ref)
         if(heading.contains(" ")){
             //https://stackoverflow.com/a/1892778
@@ -41,27 +47,56 @@ public class WebViewController {
         String newLocation = urlString + "#" + heading;
         System.out.println(newLocation);
         Platform.setImplicitExit(false);
-        //Node mark =;
         String finalHeading = heading;
         Platform.runLater(() -> {
+            /*
 
-            Element ele = (Element) webView.getEngine().getDocument().getElementById(finalHeading).getParentNode();
+            //https://stackoverflow.com/questions/52960101/how-to-edit-html-page-in-a-webview-from-javafx-without-reloading-the-page
+
+            final Document document;
+            Element ele = webView.getEngine().getDocument().getElementById(finalHeading);
             System.out.println("ele: " + ele.getTagName());
-            Element mark = webView.getEngine().getDocument().createElement("b");
+            Element mark = webView.getEngine().getDocument().createElement("mark");
             System.out.println("mrk: " + mark.getTagName());
             Element parentElement = (Element) ele.getParentNode();
             System.out.println("ele getParentNode : " + parentElement.getNodeName());
-            //ele.appendChild(mark);
             parentElement.insertBefore(mark, ele);
             mark.appendChild(ele);
             System.out.println("ele getParentNode -new: " + ele.getParentNode());
 
+             DOMSource domSource = new DOMSource(webView.getEngine().getDocument());
+
+             StreamResult result = new StreamResult(urlString);
+             TransformerFactory tf = TransformerFactory.newInstance();
+             Transformer transformer = null;
+
+             try {
+                 transformer = tf.newTransformer();
+                 transformer.setOutputProperty(OutputKeys.METHOD, "html");
+                 transformer.transform(domSource, result);
+             } catch (TransformerConfigurationException e) {
+                 e.printStackTrace();
+             } catch (TransformerException e) {
+                 e.printStackTrace();
+             }
 
 
-            //webView.getEngine().getDocument().getElementById(finalHeading)
+
+            try {
+                //https://stackoverflow.com/questions/29087077/is-it-possible-to-convert-html-into-xhtml-with-jsoup-1-8-1
+
+                document = Jsoup.parse(LocalStorageDataProvider.getHandoutFileDirectory(), "UTF-8");
+                document.outputSettings().syntax(Document.OutputSettings.Syntax.xml);
+                File outputFile = new File(LocalStorageDataProvider.getHandoutStringDirectory());
+                FileUtils.writeStringToFile(outputFile, document.html(), "UTF-8");
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }*/
+
+            
             webView.getEngine().load(newLocation);
-            webView.getEngine().reload();
-            //Attr mark = Attr;
+
         });
     }
 }
