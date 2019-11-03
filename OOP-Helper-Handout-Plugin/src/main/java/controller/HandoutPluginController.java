09199f10c2415b1e3f850142e1d29404c0735240
Marlena Wolfes <marlena.wolfes@gmx.de>
2019-11-03 11:22:53 +0100

    added provisor. check for internet connection #72

--- OOP-Helper-Handout-Plugin/src/main/java/controller/HandoutPluginController.java
+++ OOP-Helper-Handout-Plugin/src/main/java/controller/HandoutPluginController.java
@@ -7,6 +7,7 @@ import provider.HandoutContentDataProviderInterface;
 import provider.RepoLocalStorageDataProvider;
 
 import java.io.File;
+import java.io.IOException;
 
 public class HandoutPluginController implements HandoutPluginControllerInterface, OnEventListener{
     HandoutContentDataProviderInterface handoutDataProvider;
@@ -15,8 +16,29 @@ public class HandoutPluginController implements HandoutPluginControllerInterface
     public HandoutPluginController(Project project) {
         RepoLocalStorageDataProvider.setUserProjectDirectory(project);
         handoutDataProvider = ServiceManager.getService(project, HandoutContentDataProviderInterface.class);
+        checkInternetConnection();
         updateHandoutContent();
         handoutDataProvider.addListener(this);
+    }
+
+
+    public Boolean checkInternetConnection(){
+        Process process = null;
+        try {
+            process = Runtime.getRuntime().exec("ping www.geeksforgeeks.org");
+            int x = process.waitFor();
+            if (x == 0) {
+                System.out.println("Connection Successful, " + "Output was " + x);
+                return true;
+            }
+            else {
+                System.out.println("Internet Not Connected, "  + "Output was " + x);
+                return false;
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 
 
