244ca43cf4f34cc09498f612be22fe723380e885
Marlena Wolfes <marlena.wolfes@gmx.de>
2019-11-08 10:08:28 +0100

    shortcuts are displayed #71

--- OOP-Helper-Handout-Plugin/src/main/java/environment/Constants.java
+++ OOP-Helper-Handout-Plugin/src/main/java/environment/Constants.java
@@ -12,7 +12,7 @@ public class Constants {
 
     public static final String HANDOUT_FILE_NAME = "/index.html";
     public static final String CHECKLIST_FILE_NAME = "/checklist.md";
-    public static final String SHORTCUT_FILE_NAME = "/shortcut.md";
+    public static final String SHORTCUT_FILE_NAME = "/shortcuts.html";
     public static final String COMMON_ASSESSMENT_CRITERIA_FILE_NAME = "/common-assessment-criteria.md";
     public static final String SPECIFIC_ASSESSMENT_CRITERIA_FILE_NAME = "/specific-assessment-criteria.html";
 
