3d7f35436d777f9d50761cef6d7597ca96f75aaf
Marlena Wolfes <marlena.wolfes@gmx.de>
2019-11-17 00:57:20 +0100

    try fix bug when displaying notesScreen #102

--- OOP-Helper-Handout-Plugin/src/main/java/toolWindow/NotesScreen.java
+++ OOP-Helper-Handout-Plugin/src/main/java/toolWindow/NotesScreen.java
@@ -7,7 +7,7 @@ import com.intellij.openapi.actionSystem.DefaultActionGroup;
 import com.intellij.openapi.ui.SimpleToolWindowPanel;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.ui.components.JBList;
-import gui.HandoutPluginFXPanel;
+import com.intellij.ui.components.JBScrollPane;
 import objects.Notes;
 import provider.LocalStorageDataProvider;
 
@@ -16,9 +16,7 @@ import java.io.*;
 
 public class NotesScreen extends SimpleToolWindowPanel implements PluginToolWindowTabsInterface {
 
-    private HandoutPluginFXPanel noteContent;
     private ToolWindow noteToolWindow;
-    private static File content;
     private SimpleToolWindowPanel toolWindowPanel;
     private File notesFile;
     private Notes notes;
@@ -32,7 +30,9 @@ public class NotesScreen extends SimpleToolWindowPanel implements PluginToolWind
         toolWindowPanel = new SimpleToolWindowPanel(true);
         noteToolWindow = toolWindow;
         notesFile = LocalStorageDataProvider.getNotesFile();
-
+        noteContentPane = new JPanel();
+        //textArea1 = new JTextArea();
+        //notesScrollPane = new JBScrollPane();
         //TODO Write in Handler
         //https://www.mkyong.com/java/how-do-convert-java-object-to-from-json-format-gson-api/
         Gson gson = new Gson();
@@ -43,7 +43,6 @@ public class NotesScreen extends SimpleToolWindowPanel implements PluginToolWind
             e.printStackTrace();
         }
 
-
         createContent();
         initToolWindowMenu();
     }
@@ -51,7 +50,7 @@ public class NotesScreen extends SimpleToolWindowPanel implements PluginToolWind
     private void initToolWindowMenu() {
         //http://androhi.hatenablog.com/entry/2015/07/23/233932
         toolWindowPanel.setToolbar(createToolbarPanel());
-        toolWindowPanel.setContent(noteContent);
+        toolWindowPanel.setContent(noteContentPane);
     }
 
     private JComponent createToolbarPanel() {
@@ -61,32 +60,25 @@ public class NotesScreen extends SimpleToolWindowPanel implements PluginToolWind
     }
 
     private void createContent() {
-        noteContent = new HandoutPluginFXPanel();
 
         DefaultListModel dlm = new DefaultListModel();
 
         String[] content = {"Some", "Random", "Words"};
 
-        for(String word : content)
-        {
+        for(String word : content){
             dlm.addElement(word);
         }
         notesList = new JBList(dlm);
+
         textArea1.setText("dfghjkl");
         notesScrollPane.add(textArea1);
-        notesScrollPane.add(notesList);
-        //noteContentPane.add(notesScrollPane);
-        noteContent.add(notesScrollPane);
+        //notesScrollPane.add(notesList);
+        noteContentPane.add(notesScrollPane);
     }
 
     @Override
     public void updateContent() {
     }
-
-    public JComponent getToolbar(){
-        return toolWindowPanel.getToolbar();
-    }
-
 
     public JPanel getContent() {
         return toolWindowPanel;
