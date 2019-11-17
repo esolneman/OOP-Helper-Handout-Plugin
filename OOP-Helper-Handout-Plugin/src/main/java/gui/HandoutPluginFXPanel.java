c1cd696402e6cd249f33774b2dcb174a3a3612d5
Marlena Wolfes <marlena.wolfes@gmx.de>
2019-11-17 12:57:39 +0100

    setup notes screen add html editor and webView (default content) #102

--- OOP-Helper-Handout-Plugin/src/main/java/gui/HandoutPluginFXPanel.java
+++ OOP-Helper-Handout-Plugin/src/main/java/gui/HandoutPluginFXPanel.java
@@ -4,11 +4,12 @@ import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.embed.swing.JFXPanel;
+import javafx.geometry.Pos;
+import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableRow;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
+import javafx.scene.web.HTMLEditor;
 import javafx.scene.web.WebView;
 import objects.SpecificAssessmentCriteria;
 import provider.LocalStorageDataProvider;
@@ -33,5 +34,37 @@ public class HandoutPluginFXPanel extends JFXPanel {
             helloWord.setText("Hello World");
             this.setScene(new Scene(helloWord, 50, 50));
         });
+    }
+
+    public void showHTMLEditor() {
+        final HTMLEditor htmlEditor = new HTMLEditor();
+        htmlEditor.setPrefHeight(245);
+        Scene scene = new Scene(htmlEditor);
+        this.setScene(scene);
+        this.show();
+    }
+
+    //https://docs.oracle.com/javafx/2/ui_controls/editor.htm
+    public void showHTMLEditor(String urlString, WebView webView) {
+        final HTMLEditor htmlEditor = new HTMLEditor();
+        htmlEditor.setPrefHeight(250);
+
+        webView.getEngine().load(urlString);
+        webView.getEngine().setJavaScriptEnabled(true);
+
+        VBox root = new VBox();
+        root.setAlignment(Pos.CENTER);
+        ScrollPane scrollPane = new ScrollPane();
+        scrollPane.getStyleClass().add("noborder-scroll-pane");
+        scrollPane.setStyle("-fx-background-color: white");
+        scrollPane.setContent(htmlEditor);
+        scrollPane.setFitToWidth(true);
+
+        Scene scene = new Scene(new Group());
+
+        root.getChildren().addAll(webView, scrollPane);
+        scene.setRoot(root);
+        this.setScene(scene);
+        this.show();
     }
 }
--- OOP-Helper-Handout-Plugin/src/main/java/toolWindow/NotesScreen.java
+++ OOP-Helper-Handout-Plugin/src/main/java/toolWindow/NotesScreen.java
@@ -4,35 +4,45 @@ import com.google.gson.Gson;
 import com.intellij.openapi.actionSystem.ActionManager;
 import com.intellij.openapi.actionSystem.ActionToolbar;
 import com.intellij.openapi.actionSystem.DefaultActionGroup;
-import com.intellij.openapi.ui.SimpleToolWindowPanel;
 import com.intellij.openapi.wm.ToolWindow;
-import com.intellij.ui.components.JBList;
-import com.intellij.ui.components.JBScrollPane;
+import gui.HandoutPluginFXPanel;
+import javafx.application.Platform;
+import javafx.scene.web.HTMLEditor;
+import javafx.scene.web.WebView;
 import objects.Notes;
 import provider.LocalStorageDataProvider;
+import webView.WebViewController;
 
 import javax.swing.*;
 import java.io.*;
+import java.net.MalformedURLException;
 
-public class NotesScreen extends SimpleToolWindowPanel implements PluginToolWindowTabsInterface {
-
+public class NotesScreen {
+    private HandoutPluginFXPanel notesContent;
     private ToolWindow noteToolWindow;
-    private SimpleToolWindowPanel toolWindowPanel;
+   // private SimpleToolWindowPanel toolWindowPanel;
     private File notesFile;
     private Notes notes;
     private JList notesList;
     private JPanel noteContentPane;
     private JScrollPane notesScrollPane;
     private JTextArea textArea1;
+    private HTMLEditor htmlEditor;
+    private static File content;
+    private WebViewController webViewController;
+
+    private String urlString;
+    private static WebView webView;
 
     public NotesScreen(ToolWindow toolWindow) {
-        super(true, true);
-        toolWindowPanel = new SimpleToolWindowPanel(true);
+        //super(true, true);
+        //toolWindowPanel = new SimpleToolWindowPanel(true);
         noteToolWindow = toolWindow;
+        webViewController = new WebViewController();
+
         notesFile = LocalStorageDataProvider.getNotesFile();
         noteContentPane = new JPanel();
-        //textArea1 = new JTextArea();
-        //notesScrollPane = new JBScrollPane();
+
         //TODO Write in Handler
         //https://www.mkyong.com/java/how-do-convert-java-object-to-from-json-format-gson-api/
         Gson gson = new Gson();
@@ -43,24 +53,35 @@ public class NotesScreen extends SimpleToolWindowPanel implements PluginToolWind
             e.printStackTrace();
         }
 
+
+        content = LocalStorageDataProvider.getHandoutFileDirectory();
+        System.out.println("content HandoutContentScreen: " + content);
+
+        try {
+            urlString = content.toURI().toURL().toString();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
         createContent();
         initToolWindowMenu();
     }
 
     private void initToolWindowMenu() {
         //http://androhi.hatenablog.com/entry/2015/07/23/233932
-        toolWindowPanel.setToolbar(createToolbarPanel());
-        toolWindowPanel.setContent(noteContentPane);
+        //toolWindowPanel.setToolbar(createToolbarPanel());
+       // toolWindowPanel.setContent(noteContentPane);
     }
 
     private JComponent createToolbarPanel() {
         final DefaultActionGroup notesActionGroup = new DefaultActionGroup();
+        notesActionGroup.add(ActionManager.getInstance().getAction("Handout.Download"));
         final ActionToolbar actionToolbar = ActionManager.getInstance().createActionToolbar("NotesTool", notesActionGroup, true);
         return actionToolbar.getComponent();
     }
 
     private void createContent() {
-
+/*
         DefaultListModel dlm = new DefaultListModel();
 
         String[] content = {"Some", "Random", "Words"};
@@ -68,20 +89,29 @@ public class NotesScreen extends SimpleToolWindowPanel implements PluginToolWind
         for(String word : content){
             dlm.addElement(word);
         }
-        notesList = new JBList(dlm);
 
+        notesList.setModel(dlm);
         textArea1.setText("dfghjkl");
-        notesScrollPane.add(textArea1);
+        htmlEditor = new HTMLEditor();
+        notesContent.add()
+        //notesScrollPane.add(textArea1);
         //notesScrollPane.add(notesList);
-        noteContentPane.add(notesScrollPane);
+        noteContentPane.add(notesScrollPane);*/
+
+
+        notesContent = new HandoutPluginFXPanel();
+        Platform.setImplicitExit(false);
+        Platform.runLater(() -> {
+            webView = webViewController.createWebViewWithListener(urlString);
+            notesContent.showHTMLEditor(urlString, webView);
+        });
     }
 
-    @Override
-    public void updateContent() {
-    }
+    //@Override
+    public void updateContent() { }
 
-    public JPanel getContent() {
-        return toolWindowPanel;
+    public HandoutPluginFXPanel getContent() {
+        return notesContent;
     }
 
 }
