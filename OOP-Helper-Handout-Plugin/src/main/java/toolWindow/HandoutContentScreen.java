caa609e405a36e30996538645fc7df0fa734655a
Marlena Wolfes <marlena.wolfes@gmx.de>
2019-12-11 11:19:29 +0100

    fix bug that crashes when user clicks on link from code to handout

--- OOP-Helper-Handout-Plugin/src/main/java/toolWindow/HandoutContentScreen.java
+++ OOP-Helper-Handout-Plugin/src/main/java/toolWindow/HandoutContentScreen.java
@@ -1,36 +1,20 @@
 package toolWindow;
 
-import com.intellij.openapi.actionSystem.ActionManager;
-import com.intellij.openapi.actionSystem.ActionToolbar;
-import com.intellij.openapi.actionSystem.DefaultActionGroup;
-import com.intellij.openapi.actionSystem.Separator;
 import com.intellij.openapi.ui.SimpleToolWindowPanel;
 import com.intellij.openapi.wm.ToolWindow;
-import com.sun.webkit.dom.KeyboardEventImpl;
-import controller.ChecklistController;
 import controller.DownloadPDFHelper;
 import controller.LinkToHandoutController;
-import gui.NoteAddingFrame;
 import gui.PluginWebViewFXPanel;
 import javafx.application.Platform;
-import javafx.concurrent.Worker;
 import javafx.scene.web.WebView;
 import netscape.javascript.JSObject;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.events.EventListener;
-import org.w3c.dom.events.EventTarget;
-import org.w3c.dom.html.HTMLInputElement;
 import provider.LocalStorageDataProvider;
 import provider.RepoLocalStorageDataProvider;
-import provider.contentHandler.HandoutContentHandler;
-import toolWindow.actionGroups.HandoutContentActionGroup;
 import webView.WebViewController;
 
 import javax.swing.*;
 import java.io.File;
 import java.net.MalformedURLException;
-import java.util.ArrayList;
 
 public class HandoutContentScreen extends SimpleToolWindowPanel implements PluginToolWindowTabsInterface {
     private PluginWebViewFXPanel handoutContent;
@@ -61,22 +45,7 @@ public class HandoutContentScreen extends SimpleToolWindowPanel implements Plugi
 
     private void initToolWindowMenu() {
         //http://androhi.hatenablog.com/entry/2015/07/23/233932
-        //toolWindowPanel.setToolbar(createToolbarPanel());
         toolWindowPanel.setContent(handoutContent);
-    }
-
-    private JComponent createToolbarPanel() {
-        final DefaultActionGroup handoutActionGroup = new DefaultActionGroup();
-        HandoutContentActionGroup handoutContentActionGroup = (HandoutContentActionGroup) ActionManager.getInstance().getAction("Handout.TableOfContents");
-        handoutContentActionGroup.setWebViewController(webViewController);
-        ArrayList<String> headings = HandoutContentHandler.getNavHeadings();
-        handoutContentActionGroup.setHeadings(headings);
-        handoutActionGroup.add(handoutContentActionGroup);
-        handoutActionGroup.add(new Separator());
-        handoutActionGroup.add(ActionManager.getInstance().getAction("Handout.Download"));
-        handoutActionGroup.add(new Separator());
-        final ActionToolbar actionToolbar = ActionManager.getInstance().createActionToolbar("HandoutTool", handoutActionGroup, true);
-        return actionToolbar.getComponent();
     }
 
     private void createContent() {
